07dd20d1c9e41b3247069e1b9bb5e09fd7c0aac2
T3EKO <123266845+T3EKO@users.noreply.github.com>
2025-10-27 13:02:43 -0400

    Delete build directory

--- out/build/GCC 15/CMakeFiles/Balalo.dir/balalo/card.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Balalo.dir/balalo/card.cpp.o: \
- /home/lilian/Documents/projects/c/balalo/balalo/card.cpp \
- /usr/include/stdc-predef.h \
- /home/lilian/Documents/projects/c/balalo/balalo/card.h
--- out/build/GCC 15/CMakeFiles/Balalo.dir/hdsfjk/balalo.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Balalo.dir/hdsfjk/balalo.cpp.o: \
- /home/lilian/Documents/projects/c/balalo/hdsfjk/balalo.cpp \
- /usr/include/stdc-predef.h \
- /home/lilian/Documents/projects/c/balalo/hdsfjk/balalo.h
